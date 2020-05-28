8ca37698da4e93ff40f74fa9a50e0edfc61a0cfd
Fabio <flsobral@gmail.com>
2020-05-28 16:49:11 -0300

    first commit, looking nice, but the code could be better organized ^^

---
+++ src/main/java/com/totalcross/coronachart/RunCoronaChartApplication.java
@@ -0,0 +1,10 @@
+package com.totalcross.coronachart;
+
+import totalcross.TotalCrossApplication;
+
+public class RunCoronaChartApplication {
+    public static void main(String [] args) {
+        TotalCrossApplication.run(CoronaChartApp.class, 
+        "/scr", "960x520");
+    }
+}

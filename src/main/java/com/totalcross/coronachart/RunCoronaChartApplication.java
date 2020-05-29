a91d1aa50e16b6a9d28eb659dec50e2ff0568095
Fabio <flsobral@gmail.com>
2020-05-28 21:13:39 -0300

    adds a title for the chart increases the height of the simulator fixes calculation of the x/y coordinates of the control to start drawing makes the line thicker by painting the pixels adjacent to the polyline

--- src/main/java/com/totalcross/coronachart/RunCoronaChartApplication.java
+++ src/main/java/com/totalcross/coronachart/RunCoronaChartApplication.java
@@ -3,8 +3,10 @@ package com.totalcross.coronachart;
 import totalcross.TotalCrossApplication;
 
 public class RunCoronaChartApplication {
-    public static void main(String [] args) {
-        TotalCrossApplication.run(CoronaChartApp.class, 
-        "/scr", "960x520");
+    public static void main(String[] args) {
+        TotalCrossApplication.run(CoronaChartApp.class,
+                // "/scr", "1920x1280",
+                // "/scr", "960x640",
+                "/scr", "848x480");
     }
 }
